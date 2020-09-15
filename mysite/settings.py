29621e45c7a3718ffee89f9bb78fd35ba5fc0d8d
Tim <tiemoko@ualberta.ca>
2020-09-15 07:25:15 -0600

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'g$x5ik411_o-t#i#e67s=%l!h^a06_b-j*)r*i9s))!dbo#q7c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
